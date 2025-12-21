8b910abe5c73b799b251fbb7d49e6b3d2e3dc3ee
Mikhail Loginov <lmi@t-sk.ru>
2025-12-22 00:28:15 +0300

    Sprint-9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,13 +1,93 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.playlistmaker.R
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val btnBack: Button = findViewById(R.id.btnBack)
+        searchEditText = findViewById(R.id.searchEditText)
+
+        initSearchField()
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initSearchField() {
+        updateClearIcon(null)
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) = Unit
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                updateClearIcon(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // TODO: реализация поиска в следующих заданиях
+            }
+        })
+
+        searchEditText.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawableEnd = searchEditText.compoundDrawables[2]
+                if (drawableEnd != null) {
+
+                    val clearIconStart =
+                        searchEditText.width -
+                                searchEditText.paddingEnd -
+                                drawableEnd.intrinsicWidth
+
+                    if (event.x >= clearIconStart) {
+                        searchEditText.text.clear()
+                        hideKeyboard()
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    private fun updateClearIcon(text: CharSequence?) {
+        val searchIcon = getDrawable(R.drawable.ic_search)
+        val clearIcon =
+            if (text.isNullOrEmpty()) null else getDrawable(R.drawable.ic_search_clear)
+
+        searchEditText.setCompoundDrawablesWithIntrinsicBounds(
+            searchIcon,
+            null,
+            clearIcon,
+            null
+        )
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        searchEditText.clearFocus()
     }
 }
-
