babcaf994b9e973f2ad91117ec418d6324927074
Mikhail Loginov <lmi@t-sk.ru>
2025-12-23 19:34:52 +0300

    Sprint-9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,27 +2,22 @@ package com.example.playlistmaker
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.playlistmaker.R
+import androidx.core.widget.doOnTextChanged
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var searchEditText: EditText
     private var searchQuery: String = ""
 
-    companion object {
-        private const val SEARCH_QUERY_KEY = "SEARCH_QUERY_KEY"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
         val btnBack: Button = findViewById(R.id.btnBack)
         searchEditText = findViewById(R.id.searchEditText)
 
@@ -32,31 +27,15 @@ class SearchActivity : AppCompatActivity() {
 
         initSearch()
     }
+
     private fun initSearch() {
         updateClearIcon(null)
 
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) = Unit
-
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
-                searchQuery = s?.toString().orEmpty()
-                updateClearIcon(s)
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // TODO: логика поиска будет добавлена позже
-            }
-        })
+        searchEditText.doOnTextChanged { text, _, _, _ ->
+            searchQuery = text?.toString().orEmpty()
+            updateClearIcon(text)
+            // TODO: логика поиска будет добавлена позже
+        }
 
         searchEditText.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_UP) {
@@ -90,11 +69,10 @@ class SearchActivity : AppCompatActivity() {
         searchEditText.setSelection(restoredQuery.length)
     }
 
-
     private fun updateClearIcon(text: CharSequence?) {
-        val searchIcon = getDrawable(R.drawable.ic_search)
+        val searchIcon = getDrawable(R.drawable.ic_search_16)
         val clearIcon =
-            if (text.isNullOrEmpty()) null else getDrawable(R.drawable.ic_search_clear)
+            if (text.isNullOrEmpty()) null else getDrawable(R.drawable.ic_search_clear_16)
 
         searchEditText.setCompoundDrawablesWithIntrinsicBounds(
             searchIcon,
@@ -108,5 +86,9 @@ class SearchActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
         searchEditText.clearFocus()
+    }
+
+    companion object {
+        private const val SEARCH_QUERY_KEY = "SEARCH_QUERY_KEY"
     }
 }
